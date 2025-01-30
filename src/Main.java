9bd39327012190a623f371c284bd3e9de7bfbdfd
Atlas Vlad <vlad269123@gmail.com>
2025-01-30 12:51:36 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ public class Main {
         BmiService bmiService = new BmiService();
 
         // Примерные данные: вес 98 кг, рост 1.87 метра
-        double weight = 60; // вес в килограммах
-        double height = 1.75;// рост в метрах
+        double weight = 98; // вес в килограммах
+        double height = 1.87;// рост в метрах
         int bmiIndex = bmiService.calculate(weight, height);
 
         System.out.println("Индекс массы тела (BMI): " + bmiIndex);
